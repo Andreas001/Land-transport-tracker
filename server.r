049945e9a523c02edc48441197dcde8362b6f011
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-27 04:02:09 +0700

    Update server.r

--- server.r
+++ server.r
@@ -8,30 +8,21 @@ shinyServer(function(input, output)
 {
   library(jsonlite)
   
-  g <- list
-  (
-  scope = 'usa',
-  projection = list(type = 'albers usa'),
-  showland = TRUE,
-  landcolor = toRGB("gray95"),
-  subunitcolor = toRGB("gray85"),
-  countrycolor = toRGB("gray85"),
-  countrywidth = 0.5,
-  subunitwidth = 0.5
-  )
-  
   jsonFile = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
   dataFrame <- as.data.frame(jsonFile)
-    
-  output$plot <- renderPlotly({
-    dataFrame = dataFrame[1:input$count,]
-    plot_geo(dataFrame, lat = ~items.latitude, lon = ~items.longitude) %>%
-  add_markers(
-    text = ~paste(headings, id, route_id),
-    color = "read", symbol = I("square"), size = I(8), hoverinfo = "text"
-  ) %>%
-  colorbar(title = "Sucks") %>%
-  layout(
-    title = 'sucks)', geo = g
-  })
+  
+  jsonFile2 = fromJSON("http://api.metro.net/agencies/lametro/routes/704/stops/")
+  dataFrame2 <- as.data.frame(jsonFile2)
+  
+  output$plot <- renderPlotly
+  ({
+     dataFrame = dataFrame[1:input$count,]
+    plot_ly(dataFrame, x = ~items.longitude, y = ~items.latitude)
+  )}
+   
+  output$plot2 <- renderPlotly
+  ({
+     dataFrame2 = dataFrame2[1:input$count,]
+    plot_ly(dataFrame2, x = ~items.longitude, y = ~items.latitude)
+  )}
 })
