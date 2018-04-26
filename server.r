20a80b9b5f9ce5fa69d15c40cd07bde532ae670e
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-27 03:42:27 +0700

    Update server.r

--- server.r
+++ server.r
@@ -11,10 +11,7 @@ shinyServer(function(input, output)
   jsonFile = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
   dataFrame <- as.data.frame(jsonFile)
     
-  p <- plot_geo(dataFrame, lat = ~items.latitude, lon = ~items.longitude) %>%
-  add_markers(
-    color = "red", symbol = I("square"), size = I(8), hoverinfo = "text"
-  ) %>%
-  colorbar(title = "Incoming flights<br />February 2011")
-  output$plot <- renderPlotly({p$p})
+  output$plot <- renderPlotly({
+    plot_ly(dataFrame, x = ~items.longitude, y = ~items.latitude)
+  })
 })
