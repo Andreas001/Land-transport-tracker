2b91ea1170707e4b28454fd026153bbd9c1f5382
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-27 02:57:24 +0700

    Update server.r

--- server.r
+++ server.r
@@ -7,14 +7,14 @@ shinyServer(function(input, output)
 {
   library(jsonlite)
   
-  jsonFile = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
-   dataFrame <- as.data.frame(jsonFile)
-  output$map <- reactive
-  ({
-      leaflet(data = dataFrame[1:input$count,]) 
-    leaflet() %>% addTiles() %>%
-    addMarkers(~items.longitude, ~items.latitude, popup = ~as.character(items.heading), label =~as.character(items.id))
+  values <- reactiveValues(A=1)
+
+  reactiveB <- reactive({
+  values$A + 1
   })
+  
+  jsonFile = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
+  dataFrame <- as.data.frame(jsonFile)
   
   output$map <- renderLeaflet
   ({
