9eedeee29073df4f3a721e50ded0bdcf849330d3
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-27 03:22:51 +0700

    Update server.r

--- server.r
+++ server.r
@@ -10,14 +10,15 @@ shinyServer(function(input, output)
   jsonFile = fromJSON("http://api.metro.net/agencies/lametro/vehicles/")
   dataFrame <- as.data.frame(jsonFile)
   
-  dud <- reactive 
+  observe
   ({
-    output$map <- renderLeaflet
-  ({
+    invalidateLater(1000, session)
+    jsonFile = fromJSON("http://ihealth.sepdek.net/")
+    dataFrame <- as.data.frame(jsonFile)
+    
     leaflet(data = dataFrame[1:input$count,]) 
     leaflet() %>% addTiles() %>%
     addMarkers(~items.longitude, ~items.latitude, popup = ~as.character(items.heading), label =~as.character(items.id))
-  })
-  })
+ })
   
 })
