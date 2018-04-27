87b07a4a82dd0e4a175277f47364bd3ff2abc227
Andreas001 <andreasgeraldo0@gmail.com>
2018-04-27 07:28:41 +0700

    Update server.r

--- server.r
+++ server.r
@@ -8,7 +8,6 @@ shinyServer(function(input, output)
   
   output$busLoc <- renderLeaflet
   ({
-    busCount <- dataFrame(min(busLoc), max(busLoc), length.out = input$busCount + 1)
      leaflet(data = dataFrame[1:input$busCount,]) %>% addTiles() %>%
       addMarkers(~value.longitude, ~value.latitude, popup = ~as.character(value.display_name), label=~as.character(value.id))
   })
